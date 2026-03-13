ebadd766c06f78aa72fa189bef0fa2aea7d2f518
Syauqi Izzet <syauqifizan@gmail.com>
2026-03-14 03:05:45 +0700

    update create vendor

--- app/Filament/Resources/VendorResource/Pages/CreateVendor.php
+++ app/Filament/Resources/VendorResource/Pages/CreateVendor.php
@@ -2,3 +2,5 @@
 use App\Filament\Resources\VendorResource;
 use Filament\Resources\Pages\CreateRecord;
 class CreateVendor extends CreateRecord { protected static string $resource = VendorResource::class; }
+//feat: tambah fitur kecil
+    
