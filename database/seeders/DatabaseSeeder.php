ac928401e22a57d845e997b374da519596068267
Syauqi Izzet <syauqifizan@gmail.com>
2026-07-12 22:34:20 +0700

    update committ db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Buat User Admin awalan
+        // Buat User Admin awalana
         User::firstOrCreate(
             ['email' => 'admin@swift-erp.local'],
             [
